1cd409dc204dd1fd4cde59778c9d0a1e19ed5882
Ayman Ougri <103166756+aougri@users.noreply.github.com>
2022-11-18 12:34:33 -0500

    first working prototype

--- osuHitsoundsTransferProgram/src/main/java/directory/directoryHandlingUnit.java
+++ osuHitsoundsTransferProgram/src/main/java/directory/directoryHandlingUnit.java
@@ -8,12 +8,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static directory.fileParserUnit.readFile;
-import gui.OHSTP;
-import javafx.scene.control.ProgressBar;
-import javax.swing.JOptionPane;
 
 public class directoryHandlingUnit {
     public static String getSkinName(String dir) {
@@ -26,32 +25,40 @@ public class directoryHandlingUnit {
         List<File> validFiles = new ArrayList<>();
 
 
-        if (choice.equals("hitsounds") || choice.equals("h")) {
-            List<String> hitsounds = readFile(new File("src/main/java/directory/hitsounds.txt"));
-            for (File file : Objects.requireNonNull(files)) {
+        switch (choice) {
+            case "hitsounds":
+            case "h":
+                List<String> hitsounds = readFile(new File("src/main/java/directory/hitsounds.txt"));
+                for (File file : Objects.requireNonNull(files)) {
 
-                if (FilenameUtils.getExtension(String.valueOf(file)).equals("mp3") || FilenameUtils.getExtension(String.valueOf(file)).equals("ogg") || FilenameUtils.getExtension(String.valueOf(file)).equals("wav")) {
-                    if (hitsounds.contains(getSkinName(file.getName())) || file.getName().contains("soft") || file.getName().contains("normal") || file.getName().contains("hit") || file.getName().contains("combobreak")) {
-                        validFiles.add(file);
+                    if (FilenameUtils.getExtension(String.valueOf(file)).equals("mp3") || FilenameUtils.getExtension(String.valueOf(file)).equals("ogg") || FilenameUtils.getExtension(String.valueOf(file)).equals("wav")) {
+                        if (hitsounds.contains(getSkinName(file.getName())) || file.getName().contains("soft") || file.getName().contains("normal") || file.getName().contains("hit") || file.getName().contains("combobreak")) {
+                            validFiles.add(file);
+                        }
+                    }
+
+                }
+                break;
+            case "interface":
+            case "i":
+                List<String> interfacee = readFile(new File("src/main/java/directory/interface.txt"));
+                for (File file : Objects.requireNonNull(files)) {
+                    if (FilenameUtils.getExtension(String.valueOf(file)).equals("mp3") || FilenameUtils.getExtension(String.valueOf(file)).equals("ogg") || FilenameUtils.getExtension(String.valueOf(file)).equals("wav")) {
+                        if (interfacee.contains(getSkinName(file.getName()))) {
+                            validFiles.add(file);
+                        }
                     }
                 }
 
-            }
-        } else if (choice.equals("interface") || choice.equals("i")) {
-            List<String> interfacee = readFile(new File("src/main/java/directory/interface.txt"));
-            for (File file : Objects.requireNonNull(files)) {
-                if (FilenameUtils.getExtension(String.valueOf(file)).equals("mp3") || FilenameUtils.getExtension(String.valueOf(file)).equals("ogg") || FilenameUtils.getExtension(String.valueOf(file)).equals("wav")) {
-                    if (interfacee.contains(getSkinName(file.getName()))) {
+                break;
+            case "a":
+            case "all":
+                for (File file : Objects.requireNonNull(files)) {
+                    if (FilenameUtils.getExtension(String.valueOf(file)).equals("mp3") || FilenameUtils.getExtension(String.valueOf(file)).equals("ogg") || FilenameUtils.getExtension(String.valueOf(file)).equals("wav")) {
                         validFiles.add(file);
                     }
                 }
-            }
-
-        }else if(choice.equals("a") || choice.equals("all") ){
-            for(File file : Objects.requireNonNull(files)){
-            if (FilenameUtils.getExtension(String.valueOf(file)).equals("mp3") || FilenameUtils.getExtension(String.valueOf(file)).equals("ogg") || FilenameUtils.getExtension(String.valueOf(file)).equals("wav")){
-                validFiles.add(file);
-            } }
+                break;
         }
 
 
@@ -73,10 +80,13 @@ public class directoryHandlingUnit {
 
         //todo replace scanner by radio 
         
-            
+         List<File> audioFiles = new ArrayList<>();
             
         for (File file : Objects.requireNonNull(filesList)) {
             System.out.println(file.getPath());
+            if(FilenameUtils.getExtension(String.valueOf(file)).equals("mp3") || FilenameUtils.getExtension(String.valueOf(file)).equals("ogg") || FilenameUtils.getExtension(String.valueOf(file)).equals("wav")){
+                audioFiles.add(file);
+            }
         }
 
          Path result = null;
@@ -87,9 +97,8 @@ public class directoryHandlingUnit {
                     if (checkValidFiles(dir, choice).contains(file)) {
                         result = Files.move(Paths.get(file.getPath()), Paths.get(str_target + file.getName()));
                         System.out.println("Moved " + file.getName() + " to " + str_target);
-                        
-                        
                     }
+                    else if(audioFiles.isEmpty()){System.out.println("Dir has no audio files"); result = Path.of(dir);}
                 }
             } catch (IOException e) {
                 System.out.println("Exception while moving file: " + e.getMessage());
@@ -113,7 +122,7 @@ public class directoryHandlingUnit {
         for (File fileB : trgtList) {
              if(fileB.isFile()) 
              {
-                if(fileA.getName().replaceAll("\\d","").contains(fileB.getName().replaceAll("\\d","")) || fileB.getName().replaceAll("\\d","").contains(fileA.getName().replaceAll("\\d",""))){commList.add(fileA);}
+                if(fileA.getName().replaceAll("\\d","").contains(fileB.getName().replaceAll("\\d","")) || fileB.getName().replaceAll("\\d","").contains(fileA.getName().replaceAll("\\d","")) || srcList.isEmpty() || trgtDir.isEmpty()){commList.add(fileA);}
              }
          } 
     }
@@ -123,10 +132,29 @@ public class directoryHandlingUnit {
     
     public static void MoveFiles(String srcDir , String trgtDir, String choice) throws IOException{
 
-        List<File> commonFiles = FindCommonFiles(srcDir, trgtDir, choice);
+        File directory = new File(trgtDir);
+        File[] filesList = directory.listFiles();
+        
+        List<File> audioFiles = new ArrayList<>();
+            
+        for (File file : Objects.requireNonNull(filesList)) {
+            if(FilenameUtils.getExtension(String.valueOf(file)).equals("mp3") || FilenameUtils.getExtension(String.valueOf(file)).equals("ogg") || FilenameUtils.getExtension(String.valueOf(file)).equals("wav")){
+                audioFiles.add(file);
+            }
+        }
+        
+        List<File> commonFiles;
+        
+        if (!audioFiles.isEmpty() && !(audioFiles.size() < 10)) {
+            commonFiles = FindCommonFiles(srcDir, trgtDir, choice);
+        }else{
+            commonFiles = checkValidFiles(srcDir, choice);
+        } 
+        
 
         for(File file: commonFiles){
-            FileUtils.copyFile(new File(file.getAbsolutePath()), new File(trgtDir +"/"+ file.getName()));
+
+            FileUtils.copyFileToDirectory(new File(file.getAbsolutePath()), new File(trgtDir + "/"));
             System.out.println("Moved File from: "+file.getAbsolutePath()+" to " + trgtDir +"/"+ file.getName());
         }
     }
--- osuHitsoundsTransferProgram/src/main/java/directory/fileParserUnit.java
+++ osuHitsoundsTransferProgram/src/main/java/directory/fileParserUnit.java
@@ -1,61 +1,103 @@
 package directory;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Paths;
+import gui.OHSTP;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class fileParserUnit {
-    public static List<String> readFile(File file){
+    public static List<String> readFile(File file) {
         List<String> hitsounds = new ArrayList<>();
 
         try {
             File myObj = new File(file.getPath());
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                hitsounds.add(myReader.nextLine());
+            try (Scanner myReader = new Scanner(myObj)) {
+                while (myReader.hasNextLine()) {
+                    hitsounds.add(myReader.nextLine());
+                }
             }
-            myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
-            e.printStackTrace();
         }
 
         return hitsounds;
     }
 
-    public static String readOneLine(File file){
-           String temp = "";
-        try{
-        File myObj = new File(file.getPath());
-        Scanner reader = new Scanner(myObj);
-            while(reader.hasNextLine()){
-            temp += reader.nextLine();
+    public static String readOneLine(File file) {
+        StringBuilder temp = new StringBuilder();
+        try {
+            File myObj = new File(file.getPath());
+            Scanner reader = new Scanner(myObj);
+            while (reader.hasNextLine()) {
+                temp.append(reader.nextLine());
             }
-             }
-        catch(FileNotFoundException e){
-            System.out.println("an error occured");}
+        } catch (FileNotFoundException e) {
+            System.out.println("an error occured");
+        }
 
-        return temp;
+        return temp.toString();
     }
 
-
-    public static void WriteToFile(File file, String message) throws IOException {
+    public static void WriteToFile(String path, String message) throws IOException {
+        CheckIfFileExists(path);
         try {
-            FileWriter myWriter = new FileWriter(file);
+            FileWriter myWriter = new FileWriter(path);
             myWriter.write(message);
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
+        }
+    }
+
+    public static void CheckIfFileExists(String path) throws IOException {
+        File f = new File(path);
+        f.mkdir();
+
+    }
+
+    public static void write(String path) {
+
+        InputStream stream = fileParserUnit.class.getResourceAsStream(path);
+        assert stream != null;
+        System.out.println(stream.toString());
+
+    }
+
+    public static void JsonWriter(String path, String key, String value) throws IOException {
+        CheckIfFileExists(path);
+        JSONObject j = new JSONObject();
+        j.put(key, value);
+
+        try (FileWriter file = new FileWriter(path)) {
+            // We can write any JSONArray or JSONObject instance to the file
+            file.write(j.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static String JsonReader(String path, String key) {
+        JSONParser jsonParser = new JSONParser();
+
+        try {
+            Object o = jsonParser.parse(new FileReader(path));
+
+            JSONObject j = (JSONObject) o;
+
+            return (String) j.get(key);
+
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
